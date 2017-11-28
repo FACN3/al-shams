8cd16a4d0413ed54c05a2ac512ed89f5a7977b91
Marlen Awwad <marlenawsh3@gmail.com>
2017-11-28 13:10:32 +0200

    a try on testing
    related to issue #5

--- src/server.js
+++ src/server.js
@@ -1,1 +1,9 @@
-server.js
+const http = require("http");
+const port = process.env.PORT || 4000;
+const host = process.env.HOST || "localhost";
+
+const server = http.createServer();
+
+server.listen(port, () => {
+  console.log(`server listening on: http://${host}:${port}`);
+});
