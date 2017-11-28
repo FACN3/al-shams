375f824dfbd72aa5d740ed9bf3b93b18a09787ae
Amir Khoury <amirkhourykh@gmail.com>
2017-11-28 15:36:56 +0200

    Added home handler to connect index.html to the server. Reference #13.

--- src/server.js
+++ src/server.js
@@ -1,13 +1,13 @@
 
+const handler = require('./handler')
 const http = require("http");
 const port = process.env.PORT || 4000;
 const host = process.env.HOST || "localhost";
 
 
-const server = http.createServer();
+const server = http.createServer(handler);
 
 server.listen(port, () => {
 
   console.log(`server listening on: http://${host}:${port}`);
 });
-
