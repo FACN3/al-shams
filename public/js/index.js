f5be8b5bdcb28828d6e77ef80d37dce636004e39
hoslackochieng@gmail.com <hoslackochieng@gmail.com>
2017-11-30 01:59:39 +0200

    pictures change with weather condition Reference #37

--- public/js/index.js
+++ public/js/index.js
@@ -11,23 +11,48 @@ function showWeather() {
 	  var tempnode = document.createElement('h2');
 	  tempnode.id = 'temp';
 	  document.body.appendChild(tempnode);
+    var sunny = document.querySelector('#sunny');
+    var rainy = document.querySelector('#rainy');
+    var clouds = document.querySelector('#clouds');
 
 
   
   xhr.onreadystatechange = function() {
     if ( xhr.status == 200 && xhr.readyState==4) {
-      data = JSON.parse(xhr.responseText);
-      console.log(data);
-    }
-  
-  	
-  	document.getElementById('temp').innerHTML = Math.round(data.main.temp-273) + '&#8451;';
-  	
+        data = JSON.parse(xhr.responseText);
+        console.log(data);
+        console.log('this is the sky status',data.weather[0].main);
+        if(data.weather[0].main == 'Clear'){
+            sunny.style.display = 'block';
+            rainy.style.display = 'none';
+            clouds.style.display = 'none';
+      }
+      else if(data.weather[0].main == 'Rain' || data.weather[0].main == 'Mist'){
+              rainy.style.display = 'block';
+              sunny.style.display = 'none';
+              clouds.style.display = 'none';   
+      }
+      else if(data.weather[0].main == 'Clouds'){
+              clouds.style.display = 'block'; 
+              rainy.style.display = 'none';
+              sunny.style.display = 'none';
+
+      }else{
+            alert('No weather type');
+            clouds.style.display = 'block'; 
+            rainy.style.display = 'block';
+            sunny.style.display = 'block';
+
+      }
+
+      document.getElementById('temp').innerHTML = Math.round(data.main.temp-273) + '&#8451;';
+  }
+
+     		
 
   };
  
    xhr.open("GET", "/api?search=" + query, true);
-    //xhr.open('GET','https://www.google.co.il/search?client=ubuntu&chan',true);
     xhr.send();
 
 }
